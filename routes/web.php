13b4649b716a8210fdf64d70cfd4a554ce6a2f0a
HMHHBI <hmhhbi@gmail.com>
2026-04-18 21:25:54 +0500

    update: Remove redundant routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Category;
-
-Route::get('/setup-data', function () {
-    if (Category::count() == 0) {
-        // 'title' ki jagah 'name' use karein
-        Category::create(['name' => 'Technology', 'slug' => 'technology']);
-        Category::create(['name' => 'Lifestyle', 'slug' => 'lifestyle']);
-        Category::create(['name' => 'AI', 'slug' => 'ai']);
-    }
-    return "Categories created successfully with correct field names!";
-});
 
 Route::get('/', function () {
     return view('welcome');
